9685c2181c9bcd0f5c525d113d0110f915e075be
Luan Lima <lordellu@hotmail.com>
2022-04-22 16:17:50 -0300

    Projeto criado

---
+++ src/test/java/br/com/cursonelio/projspring/ProjSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cursonelio.projspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
